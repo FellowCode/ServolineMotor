b36869c51282ff80052d9fe109e1f2958c8f1963
FellowCode <31797522+FellowCode@users.noreply.github.com>
2018-10-05 00:14:58 +1000

    update

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ from kivy.uix.switch import Switch
 
 from kivy.config import Config
 
-window_height = 280
+window_height = 320
 window_width = 400
+
+
 
 Config.set('graphics', 'resizable', 0)
 Config.set('graphics', 'width', window_width)
@@ -53,7 +55,74 @@ class ParamInput(TextInput):
         self.multiline = False
         self.padding = [6, 2]
 
+
+
 class ServolineMotorApp(App):
+    auto_mode = True
+
+    def change_mode(self):
+        self.auto_mode = not self.auto_mode
+        self.floatlayout.update()
+
+    def build_auto_mode(self):
+        fl_mode = FloatLayout()
+        fl_mode.size_hint = (1, None)
+        fl_mode.height = window_height - 255
+        fl_mode.pos = (0, window_height - 255)
+
+        start_button = Button()
+        start_button.size_hint = (None, None)
+        start_button.text = 'Старт'
+        start_button.size = (90, 30)
+        start_button.pos = (40, window_height - 265)
+
+        stop_button = Button()
+        stop_button.size_hint = (None, None)
+        stop_button.text = 'Стоп'
+        stop_button.size = (90, 30)
+        stop_button.pos = (140, window_height - 265)
+
+        mode_button = Button()
+        mode_button.size_hint = (None, None)
+        mode_button.text = 'Ручной режим'
+        mode_button.size = (120, 30)
+        mode_button.pos = (240, window_height - 265)
+
+        fl_mode.add_widget(start_button)
+        fl_mode.add_widget(stop_button)
+        fl_mode.add_widget(mode_button)
+
+        return fl_mode
+
+    def build_manual_mode(self):
+        fl_mode = FloatLayout()
+        fl_mode.size_hint = (1, None)
+        fl_mode.height = window_height - 255
+        fl_mode.pos = (0, window_height - 255)
+
+        left_button = Button()
+        left_button.size_hint = (None, None)
+        left_button.text = '<--'
+        left_button.size = (90, 30)
+        left_button.pos = (40, window_height - 265)
+
+        right_button = Button()
+        right_button.size_hint = (None, None)
+        right_button.text = '-->'
+        right_button.size = (90, 30)
+        right_button.pos = (140, window_height - 265)
+
+        mode_button = Button()
+        mode_button.size_hint = (None, None)
+        mode_button.text = 'Авто режим'
+        mode_button.size = (120, 30)
+        mode_button.pos = (240, window_height - 265)
+
+        fl_mode.add_widget(left_button)
+        fl_mode.add_widget(right_button)
+        fl_mode.add_widget(mode_button)
+
+        return fl_mode
 
     def build(self):
         floatlayout = FloatLayout()
@@ -99,11 +168,17 @@ class ServolineMotorApp(App):
         decceleration_time_input = ParamInput()
         decceleration_time_input.pos = (210, window_height-140)
 
+        work_time_label = ParamLabel('Время работы (мс)')
+        work_time_label.pos = (30, window_height-170)
+
+        work_time_input = ParamInput()
+        work_time_input.pos = (210, window_height-170)
+
         apply_param_button = Button()
         apply_param_button.text = 'Применить параметры'
         apply_param_button.size_hint = (.8, None)
         apply_param_button.height = 30
-        apply_param_button.pos = (200 - 400*.8*.5, window_height - 190)
+        apply_param_button.pos = (200 - 400*.8*.5, window_height - 220)
 
         floatlayout.add_widget(com_input)
         floatlayout.add_widget(com_label)
@@ -112,14 +187,24 @@ class ServolineMotorApp(App):
         floatlayout.add_widget(speed_label)
         floatlayout.add_widget(acceleration_time_label)
         floatlayout.add_widget(decceleration_time_label)
+        floatlayout.add_widget(work_time_label)
 
         floatlayout.add_widget(speed_input)
         floatlayout.add_widget(acceleration_time_input)
         floatlayout.add_widget(decceleration_time_input)
+        floatlayout.add_widget(work_time_input)
 
         floatlayout.add_widget(connect_button)
         floatlayout.add_widget(apply_param_button)
+
+        if self.auto_mode:
+            floatlayout.add_widget(self.build_auto_mode())
+        else:
+            floatlayout.add_widget(self.build_manual_mode())
+
         # return a Button() as a root widget
+        self.floatlayout = floatlayout
+
         return floatlayout
 
 
