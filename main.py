6242be7d934dd9dfc6ce8733080833251ed24265
FellowCode <31797522+FellowCode@users.noreply.github.com>
2018-10-04 21:01:13 +1000

    update

--- main.py
+++ main.py
@@ -1,42 +1,126 @@
 import kivy
+import copy
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.floatlayout import FloatLayout
 
 from serial.tools import list_ports
-print(list_ports.comports())
 
 from kivy.uix.dropdown import DropDown
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
+from kivy.uix.switch import Switch
 
 from kivy.config import Config
 
-Config.set('graphics', 'resizable', 0)
-Config.set('graphics', 'width', 500)
-Config.set('graphics', 'height', 320)
+window_height = 280
+window_width = 400
 
+Config.set('graphics', 'resizable', 0)
+Config.set('graphics', 'width', window_width)
+Config.set('graphics', 'height', window_height)
+Config.write()
+
+
+class BaseLabel(Label):
+    def __init__(self, text):
+        super().__init__()
+        self.size_hint = (None, None)
+        self.width = 40
+        self.height = 20
+        self.font_size = '14sp'
+        self.text = text
+
+class ParamLabel(BaseLabel):
+    def __init__(self, text):
+        super().__init__(text)
+        self.text_size = (200, 20)
+        self.width = 120
+        self.halign = 'right'
+
+class ParamInput(TextInput):
+    def __init__(self):
+        super().__init__()
+        self.size_hint = (None, None)
+        self.width = 70
+        self.height = 20
+        self.font_size = '12sp'
+        self.line_height = 20
+        self.multiline = False
+        self.padding = [6, 2]
 
 class ServolineMotorApp(App):
 
     def build(self):
-        com_dropdown = DropDown()
-        for i in range(3):
-            com_dropdown.add_widget(Button(text=str(i), height=20))
+        floatlayout = FloatLayout()
 
-        bl_main = BoxLayout(orientation='vertical', padding=10)
-        fl1 = AnchorLayout(anchor_x='left', anchor_y='center')
-        com_input = TextInput()
-        fl1.add_widget(com_input)
-        bl_main.add_widget(fl1)
-        bl_main.add_widget(Widget())
-        bl_main.add_widget(Widget())
-        bl_main.add_widget(Widget())
-        bl_main.add_widget(Widget())
+        com_input = ParamInput()
+        com_input.width = 30
+        com_input.pos = (60, window_height-30)
+
+        com_label = BaseLabel('COM')
+        com_label.pos = (10, window_height-30)
+
+        connect_button = Button()
+        connect_button.size_hint = (None, None)
+        connect_button.size = (90, 25)
+        connect_button.text = 'Соединиться'
+        connect_button.font_size = '12sp'
+        connect_button.pos = (100, window_height-33)
+        connect_button.padding = [5, 2]
+
+        motor_state_label = BaseLabel('Motor')
+        motor_state_label.pos = (window_width-150, window_height-30)
+
+        motor_switch = Switch()
+        motor_switch.active = False
+        motor_switch.size_hint = (None, None)
+        motor_switch.pos = (300, window_height-70)
+
+        speed_label = ParamLabel('Скорость (об/мин)')
+        speed_label.pos = (30, window_height-80)
+
+        speed_input = ParamInput()
+        speed_input.pos = (210, window_height-80)
+
+        acceleration_time_label = ParamLabel('Время ускорения (мс)')
+        acceleration_time_label.pos = (30, window_height-110)
+
+        acceleration_time_input = ParamInput()
+        acceleration_time_input.pos = (210, window_height-110)
+
+        decceleration_time_label = ParamLabel('Время торможения (мс)')
+        decceleration_time_label.pos = (30, window_height-140)
+
+        decceleration_time_input = ParamInput()
+        decceleration_time_input.pos = (210, window_height-140)
+
+        apply_param_button = Button()
+        apply_param_button.text = 'Применить параметры'
+        apply_param_button.size_hint = (.8, None)
+        apply_param_button.height = 30
+        apply_param_button.pos = (200 - 400*.8*.5, window_height - 190)
+
+        floatlayout.add_widget(com_input)
+        floatlayout.add_widget(com_label)
+        floatlayout.add_widget(motor_state_label)
+        floatlayout.add_widget(motor_switch)
+        floatlayout.add_widget(speed_label)
+        floatlayout.add_widget(acceleration_time_label)
+        floatlayout.add_widget(decceleration_time_label)
+
+        floatlayout.add_widget(speed_input)
+        floatlayout.add_widget(acceleration_time_input)
+        floatlayout.add_widget(decceleration_time_input)
+
+        floatlayout.add_widget(connect_button)
+        floatlayout.add_widget(apply_param_button)
         # return a Button() as a root widget
-        return bl_main
+        return floatlayout
 
 
 if __name__ == '__main__':
